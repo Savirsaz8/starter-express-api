49c4b5035b36f01912db5b23b7e1c0a189a9bd79
Savirsaz8 <savira.aliva@gmail.com>
2023-08-04 14:02:53 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
+const bodyParser = require('body-parser'); // Import the body-parser module
 const app = express()
-app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.all('/', (req, res) => {
     console.log("Just got a request!")
     console.log(req.body)
