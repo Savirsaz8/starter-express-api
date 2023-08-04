098e45202e9da0eabb5152ac250d44d71b8d5a22
Savirsaz8 <savira.aliva@gmail.com>
2023-08-04 13:40:07 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 app.all('/', (req, res) => {
     console.log("Just got a request!")
-    res.send('Yo!')
+    console.log(req.body)
+    res.send('RECEIVEOK')
 })
 app.listen(process.env.PORT || 3000)
